4226331f00e81bd6ab4483d5a65426ba0399f5fe
Kerem1989 <98690898+Kerem1989@users.noreply.github.com>
2025-05-05 19:01:09 +0200

    Secure env vars with dotenv, clean test setup

--- src/test/java/com/sveacms/SveaCmsApplicationTests.java
+++ src/test/java/com/sveacms/SveaCmsApplicationTests.java
@@ -11,14 +11,17 @@ class sveaCMSApplicationTests {
 
     @DynamicPropertySource
     static void overrideProps(DynamicPropertyRegistry registry) {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
-        registry.add("spring.datasource.url", () -> dotenv.get("DB_URL"));
-        registry.add("spring.datasource.username", () -> dotenv.get("DB_USERNAME"));
-        registry.add("spring.datasource.password", () -> dotenv.get("DB_PASSWORD"));
+        registry.add("spring.datasource.url", () -> dotenv.get("DB_URL", System.getenv("DB_URL")));
+        registry.add("spring.datasource.username", () -> dotenv.get("DB_USERNAME", System.getenv("DB_USERNAME")));
+        registry.add("spring.datasource.password", () -> dotenv.get("DB_PASSWORD", System.getenv("DB_PASSWORD")));
     }
 
     @Test
     void contextLoads() {
     }
 }
+
